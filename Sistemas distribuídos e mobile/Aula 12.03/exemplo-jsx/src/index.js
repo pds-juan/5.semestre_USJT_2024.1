7e13c7bdcf952391ed47e6e33ba2cf11bc791423
Juan <juan02pires@gmail.com>
2024-03-23 23:40:56 -0300

    primeiro componente react

---
+++ Sistemas distribuídos e mobile/Aula 12.03/exemplo-jsx/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return <div>Meu primeiro componente ReactJS</div>
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
