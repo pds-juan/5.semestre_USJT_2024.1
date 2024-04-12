981f0b3d664e63ece1ac76aab17e28e5f082f886
Juan <juan02pires@gmail.com>
2024-04-11 23:53:50 -0300

    dois componentes com react

---
+++ Sistemas distribuídos e mobile/Aula 09.04/props/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Titulo from "./Titulo"
+import Corpo from "./Corpo"
+
+const App = () => {
+    return (
+        <div>
+            <Titulo />
+            <Corpo />
+        </div>
+    )
+}
+
+export default App
---
+++ Sistemas distribuídos e mobile/Aula 09.04/props/src/components/Corpo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Corpo = () => {
+    return (
+        <ul>
+            <li>
+                Pedido 01
+            </li>
+            <li>
+                Pedido 02
+            </li>
+            <li>
+                Pedido 03
+            </li>
+        </ul>
+    )
+}
+
+export default Corpo
