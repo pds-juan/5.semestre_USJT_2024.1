5cc5078b745b57bdcd0d4455573f74895c2f045a
Juan <juan02pires@gmail.com>
2024-04-12 23:54:31 -0300

    microsservico de lembretes

---
+++ Sistemas distribuídos e mobile/Aula 01.04/comandos_terminal.js
@@ -0,0 +1,11 @@
+// npm init -y
+
+// npm install axios
+// npm install cors
+// npm install express
+
+// ou os 3 de uma vez
+// npm install axios cors express
+
+// instalando o nodemon apenas para desenvolvimento
+// npm install nodemon --save-dev
