2854b94459013d182f673cee1bc5bd1ce4a55f9a
Juan <juan02pires@gmail.com>
2024-04-22 23:45:00 -0300

    microsservico de observacoes

---
+++ Sistemas distribuídos e mobile/Aula 22.04/observacoes/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+// npm install uuid
+const { v4: uuvid4 } = require('uuid');
+
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+
+const observacoesPorLembreteId = {};
+
+// :id é um placeholder
+// exemplo => /lembretes/123456.../observacoes
+app.post('/lembretes/:id/observacoes', async (req, res) => {
+    const idObs = uuvid4();
+    const { texto } = req.body;
+    // req.params dá acesso à lista de parâmetros da URL
+    const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || [];
+    observacoesDoLembrete.push({ id: idObs, texto });
+    observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
+
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "ObservacaoCriada",
+        dados: {
+            id: idObs, texto, lembreteId: req.params.id
+        }
+    });
+    res.status(201).send(observacoesDoLembrete);
+});
+
+app.get('/lembretes/:id/observacoes', (req, res) => {
+    res.send(observacoesPorLembreteId[req.params.id] || []);
+});
+
+app.post('/eventos', (req, res) => {
+    console.log(req.body);
+    res.send({ msg: "ok" });
+});
+
+app.listen(5000, () => {
+    console.log('Observações. Porta 5000.')
+});
